f74a7577c2b7f664a9c71c15edba5a8e7682f999
Sébastien Gaya <sebastien.gaya@gmail.com>
2020-04-27 14:37:07 +0200

    bundle + handle blocks

--- lib/deep_merge_existing_keys/core_ext/hash.rb
+++ lib/deep_merge_existing_keys/core_ext/hash.rb
@@ -1,12 +1,19 @@
 class Hash
-  def deep_merge_existing_keys(other_hash)
-    dup.deep_merge_existing_keys!(other_hash)
+  def deep_merge_existing_keys(other_hash, &block)
+    dup.deep_merge_existing_keys!(other_hash, &block)
   end
 
-  def deep_merge_existing_keys!(other_hash)
+  def deep_merge_existing_keys!(other_hash, &block)
     other_hash.each_pair do |k,v|
       next unless self.keys.include? k
-      self[k] = self[k].is_a?(Hash) && v.is_a?(Hash) ? self[k].deep_merge_existing_keys(v) : v
+
+      self[k] = if self[k].is_a?(Hash) && v.is_a?(Hash)
+                  self[k].deep_merge_existing_keys(v, &block)
+                elsif block_given?
+                  block.call(k, self[k], v)
+                else
+                  v
+                end
     end
     self
   end
